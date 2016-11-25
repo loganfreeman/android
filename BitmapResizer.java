e7fee35fc4c5c819ddb269a5a53e04fdb660a233
loganfreeman <shanhongcheng@hotmail.com>
2016-11-25 10:08:27 -0700

    Create BitmapResizer.java

---
+++ BitmapResizer.java
@@ -0,0 +1,25 @@
+public class BitmapResizer {
+
+    public static Bitmap ShrinkBitmap(String file, int width, int height) {
+
+        BitmapFactory.Options bmpFactoryOptions = new BitmapFactory.Options();
+        bmpFactoryOptions.inJustDecodeBounds = true;
+        Bitmap bitmap = BitmapFactory.decodeFile(file, bmpFactoryOptions);
+
+        int heightRatio = (int) Math.ceil(bmpFactoryOptions.outHeight / (float) height);
+        int widthRatio = (int) Math.ceil(bmpFactoryOptions.outWidth / (float) width);
+
+        if (heightRatio > 1 || widthRatio > 1) {
+            if (heightRatio > widthRatio) {
+                bmpFactoryOptions.inSampleSize = heightRatio;
+            } else {
+                bmpFactoryOptions.inSampleSize = widthRatio;
+            }
+        }
+
+        bmpFactoryOptions.inJustDecodeBounds = false;
+        bitmap = BitmapFactory.decodeFile(file, bmpFactoryOptions);
+
+        return bitmap;
+    }
+}
