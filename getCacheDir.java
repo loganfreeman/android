ea0d4b738cd00fe13e4009eda23f0ecdb988c19d
loganfreeman <shanhongcheng@hotmail.com>
2016-12-08 09:57:02 -0700

    Create getCacheDir.java

---
+++ getCacheDir.java
@@ -0,0 +1,9 @@
+public File getCacheDir() {
+        if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+            File cacheDir = getExternalCacheDir();
+            if (cacheDir != null && (cacheDir.exists() || cacheDir.mkdirs())) {
+                return cacheDir;
+            }
+        }
+        return super.getCacheDir();
+    }
